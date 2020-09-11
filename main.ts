6a21b4e0b198cbb92f35573561e61e17609eea6c
ExOster <71136452+ExOster@users.noreply.github.com>
2020-09-11 17:30:39 +0200

    klocka samt

--- main.ts
+++ main.ts
@@ -6,13 +6,13 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    tid = "" + timmar + (":" + minuter)
-    basic.showString(tid)
+    Spela_ljud = 0
 })
 radio.onReceivedString(function (receivedString) {
     if (true) {
         radio.setGroup(12)
         radio.sendValue("Sol", 1)
+        Spela_ljud = 1
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -22,15 +22,25 @@ input.onButtonPressed(Button.B, function () {
         timmar = 0
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    radio.setGroup(11)
+})
+let Spela_ljud = 0
 let timmar = 0
 let minuter = 0
-let tid = ""
 radio.setGroup(11)
-let AMPM = false
-tid = ""
+let tid = ""
 let adjust = 0
 minuter = 0
 timmar = 0
+basic.forever(function () {
+    tid = "" + timmar + (":" + minuter)
+    basic.showString(tid)
+    basic.pause(5000)
+    if (Spela_ljud == 1) {
+        music.playMelody("C5 B A G F E D C ", 1000)
+    }
+})
 basic.forever(function () {
     basic.pause(60000)
     if (minuter < 59) {
